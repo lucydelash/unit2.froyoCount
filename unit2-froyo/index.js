2223d86142af4b7f2c8aad5e8786ef9af4954b30
lucydelash <lucyrudd614@gmail.com>
2024-03-12 15:48:34 -0400

    added js code for froyoCount

---
+++ unit2-froyo/index.js
@@ -0,0 +1,22 @@
+// Prompt the user to enter a list of (comma-separated) froyo flavors
+const userInput = prompt("enter a list of flavors (separated by commas):");
+
+// Split the input string into an array of flavors
+let flavors = userInput.split(",");
+
+// Create an object to store the # of each flavor
+const flavorCounts = {};
+
+// Count how many scoops of each flavor
+flavors.forEach(function(flavor) {
+    flavor = flavor.trim(); // Removing leading/trailing spaces
+    if (flavor in flavorCounts) {
+        flavorCounts[flavor]++;
+    } else {
+        flavorCounts[flavor] = 1;
+    }
+});
+
+// Display the scoop count in the console
+console.log("Flavor Counts:");
+console.table(flavorCounts);
